10e1a7c94b661ff12f91ca59e0309c9116775598
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2022-04-27 22:53:51 +0530

    Input key Space Done

---
+++ KeyInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Day10 : MonoBehaviour
+{
+    private int point;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("space"))
+            {
+            print("pras the space");
+
+        }
+    }
+}
